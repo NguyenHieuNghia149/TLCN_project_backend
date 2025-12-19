6a3acf0d22c84e20867530c6d313da2543da1441
NguyenHieuNghia149 <hieunghia484@gmail.com>
2025-12-19 23:44:09 +0700

    refactor: fix edit challenge add testcase

--- src/services/challenge.service.ts
+++ src/services/challenge.service.ts
@@ -420,7 +420,10 @@ export class ChallengeService {
 
     // Handle testcases update if provided
     if (updateData.testcases) {
+      console.log('Updating testcases:', updateData.testcases.length);
       await this.testcaseRepository.updateTestcasesTransactional(challengeId, updateData.testcases);
+    } else {
+      console.log('No testcases provided in updateData');
     }
 
     return this.getChallengeById(challengeId);
