3bfd906e63b9c67440680e71dae17a4bddce104a
NguyenHieuNghia149 <hieunghia484@gmail.com>
2025-12-23 16:44:21 +0700

    feat: Add initial Express server setup with middleware, database connection, and service integrations.

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,12 @@ app.use(
   })
 );
 
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3001'];
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://app.algoforge.site',
+  'https://sandbox.algoforge.site',
+  'https://api.algoforge.site',
+];
 
 app.use(
   cors({
