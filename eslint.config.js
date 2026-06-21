e38fcf0cd1d07ad7eb813a5559a9e9c86a8957af
NguyenHieuNghia149 <hieunghia484@gmail.com>
2026-06-21 22:23:32 +0700

    feat: add ESLint configuration for TypeScript with recommended settings

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  resolvePluginsRelativeTo: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+module.exports = [
+  ...compat.config({
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      project: './tsconfig.json',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      '@typescript-eslint/no-var-requires': 'error',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'prefer-const': 'error',
+      'no-var': 'error',
+    },
+    env: {
+      node: true,
+      es2022: true,
+      jest: true,
+    },
+    ignorePatterns: ['**/dist/**', 'node_modules/**', 'build/', 'coverage/', '*.min.js'],
+  }),
+];
